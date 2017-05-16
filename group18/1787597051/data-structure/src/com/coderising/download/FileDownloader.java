d07b82a36a5cc3598bca7d3a3cf4e2fbce28ef58
core2for <Baisicscoreformy@outlook.com>
2017-05-16 12:57:53 +0800

    add fd.java and test code

--- group18/1787597051/data-structure/src/com/coderising/download/FileDownloader.java
+++ group18/1787597051/data-structure/src/com/coderising/download/FileDownloader.java
@@ -25,21 +25,21 @@ public class FileDownloader {
 	}
 
 	public void execute() {
-		// ������ʵ����Ĵ��룬 ע�⣺ ��Ҫ�ö��߳�ʵ������
-		// ��������������������ӿ�, ����Ҫд�⼸���ӿڵ�ʵ�ִ���
-		// (1) ConnectionManager , ���Դ�һ�����ӣ�ͨ��Connection���Զ�ȡ���е�һ�Σ���startPos,
-		// endPos��ָ����
-		// (2) DownloadListener, �����Ƕ��߳����أ� ���������Ŀͻ��˲�֪��ʲôʱ���������������Ҫʵ�ֵ�����
-		// �̶߳�ִ�����Ժ� ����listener��notifiedFinished������ �����ͻ��˾����յ�֪ͨ��
-		// �����ʵ��˼·��
-		// 1. ��Ҫ����ConnectionManager��open���������ӣ�
-		// Ȼ��ͨ��Connection.getContentLength��������ļ��ĳ���
-		// 2. ��������3���߳����أ� ע��ÿ���߳���Ҫ�ȵ���ConnectionManager��open����
-		// Ȼ�����read������ read�������ж�ȡ�ļ��Ŀ�ʼλ�úͽ���λ�õĲ����� ����ֵ��byte[]����
-		// 3. ��byte����д�뵽�ļ���
-		// 4. ���е��̶߳���������Ժ� ��Ҫ����listener��notifiedFinished����
+		// 在这里实现你的代码， 注意： 需要用多线程实现下载
+				// 这个类依赖于其他几个接口, 你需要写这几个接口的实现代码
+				// (1) ConnectionManager , 可以打开一个连接，通过Connection可以读取其中的一段（用startPos,
+				// endPos来指定）
+				// (2) DownloadListener, 由于是多线程下载， 调用这个类的客户端不知道什么时候结束，所以你需要实现当所有
+				// 线程都执行完以后， 调用listener的notifiedFinished方法， 这样客户端就能收到通知。
+				// 具体的实现思路：
+				// 1. 需要调用ConnectionManager的open方法打开连接，
+				// 然后通过Connection.getContentLength方法获得文件的长度
+				// 2. 至少启动3个线程下载， 注意每个线程需要先调用ConnectionManager的open方法
+				// 然后调用read方法， read方法中有读取文件的开始位置和结束位置的参数， 返回值是byte[]数组
+				// 3. 把byte数组写入到文件中
+				// 4. 所有的线程都下载完成以后， 需要调用listener的notifiedFinished方法
 
-		// ����Ĵ�����ʾ�����룬 Ҳ����˵ֻ��һ���̣߳� ����Ҫ����ɶ��̵߳ġ�
+				// 下面的代码是示例代码， 也就是说只有一个线程， 你需要改造成多线程的。
 		Connection conn = null;
 		try {
 
